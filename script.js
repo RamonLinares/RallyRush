a042f8e3b484267f42d5c016ac91dc8bb7311258
RamonLinares <ramonlinares@gmail.coms>
2024-08-15 18:51:36 +0200

    touch controls
    it should be playable in mobile devices

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (function () {
-    
+
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     const renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -41,6 +41,51 @@
         if (controlChanged) {
             gameManager.setControls(controls);
         }
+    });
+    // Touch control setup
+    const touchStartX = 0;
+    const touchEndX = 0;
+    const touchStartY = 0;
+    const touchEndY = 0;
+
+    document.addEventListener('touchstart', e => {
+        touchStartX = e.changedTouches[0].screenX;
+        touchStartY = e.changedTouches[0].screenY;
+    });
+
+    document.addEventListener('touchmove', e => {
+        touchEndX = e.changedTouches[0].screenX;
+        touchEndY = e.changedTouches[0].screenY;
+
+        const deltaX = touchEndX - touchStartX;
+        const deltaY = touchEndY - touchStartY;
+
+        if (Math.abs(deltaX) > Math.abs(deltaY)) {
+            if (deltaX > 0) {
+                controls.right = true;
+                controls.left = false;
+            } else {
+                controls.left = true;
+                controls.right = false;
+            }
+        } else {
+            if (deltaY > 0) {
+                controls.brake = true;
+                controls.accelerate = false;
+            } else {
+                controls.accelerate = true;
+                controls.brake = false;
+            }
+        }
+        gameManager.setControls(controls);
+    });
+
+    document.addEventListener('touchend', e => {
+        controls.left = false;
+        controls.right = false;
+        controls.accelerate = false;
+        controls.brake = false;
+        gameManager.setControls(controls);
     });
 
     document.getElementById('startButton').addEventListener('click', () => gameManager.startGame());
